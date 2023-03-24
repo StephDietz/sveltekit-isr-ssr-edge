d8394d10c9c814b738b5b0dc9021ede3f32cb42b
StephDietz <steph.dietz@vercel.com>
2023-03-23 19:56:44 -0500

    update ISR date

--- src/routes/isr/+page.server.js
+++ src/routes/isr/+page.server.js
@@ -7,7 +7,16 @@ export const config = {
 };
 
 export function load() {
-	const generatedAt = new Date().toISOString();
+	const generatedAt = new Date().toLocaleString('en-GB', {
+		day: '2-digit',
+		month: '2-digit',
+		year: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+		fractionalSecondDigits: 3 // for milliseconds
+	});
+
 	return {
 		generatedAt
 	};
