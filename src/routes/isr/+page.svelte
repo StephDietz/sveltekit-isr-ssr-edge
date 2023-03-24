b253d95aa8bcc2c89d6ba35773d918c0dc5c08bc
StephDietz <steph.dietz@vercel.com>
2023-03-23 20:07:44 -0500

    isr text update

--- src/routes/isr/+page.svelte
+++ src/routes/isr/+page.svelte
@@ -7,10 +7,14 @@
 	<div class="font-bold text-2xl mb-4">Incremental Static Regeneration</div>
 	<div class="opacity-80 mb-4">
 		This page is returning the date from a SvelteKit page endpoint with ISR configuration specified.
-		Vercel supports Incremental Static Regeneration (ISR), which provides the performance and cost
-		advantages of prerendered content with the flexibility of dynamically rendered content.
-		Basically, after the page is initially generated, the server will consider the page "stale" and
-		in need of regeneration after a certain amount of time has passed.
+		Incremental Static Regeneration allows you to create or update content without redeploying your
+		site. When you deploy a route with ISR, Vercel caches the page to serve it to visitors
+		statically, and rebuilds it on a time interval of your choice. ISR has three main benefits for
+		developers: better performance, improved security, and faster build times. Learn more about ISR <a
+			href="https://vercel.com/docs/concepts/incremental-static-regeneration/overview#"
+			target="_blank"
+			class="text-blue-400 hover:text-blue-500">here</a
+		>.
 	</div>
 	<div class="opacity-80 mb-4">
 		This page endpoint runs on the server, and returns the current date and time. Since it is
